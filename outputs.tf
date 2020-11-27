45d27474762273f7bb7adee4c2c60a6b093e991a
Lilly Holden <lholden@adaptavist.com>
2020-11-27 14:40:32 +0000

    chore: fixed output params

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,3 @@
-output "workload_route53_record_name" {
-  description = "The name of the record in the workload account"
-  value       = aws_route53_record.workload.name
-}
-
-output "workload_route53_record_fqdn" {
-  description = "The FQDN of the record in the workload account"
-  value       = aws_route53_record.workload.fqdn
-}
-
 output "main_route53_record_name" {
   description = "The name of the record in the main account"
   value       = aws_route53_record.main.name
